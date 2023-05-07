d4e9cf057274a169ce9a41eb3a387d607dedac8d
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-05-07 23:49:52 +0530

    SPA Authentication - Signing users in.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
-        //
+        $user = User::where('email', $request->email)->first();
+
+        if( !$user || Hash::check($request->password, $user->password) ) {
+            throw ValidationException::withMessages([
+                'email' => ['The credentials you entered are incorrect.']
+            ]);
+        }
     }
 }
