868b979dfe14d195cee1d38aa05534bcdcc5d7ec
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-05-07 07:38:02 +0530

    Typo correction, "php artisan migrate --seed".

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -18,7 +18,7 @@ class TaskFactory extends Factory
     {
         return [
             'name' => fake()->sentence(),
-            'is_complated' => rand(0, 1)
+            'is_completed' => rand(0, 1)
         ];
     }
 }
